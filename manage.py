39f5db66ee46e179c870cbabad7721760b49c7c7
ajoshi1 <talk2ambarish@gmail.com>
2016-06-29 15:03:43 -0700

    updated the right port and host binding of the Flask app

--- manage.py
+++ manage.py
@@ -2,12 +2,13 @@ from ivr_phone_tree_python import app
 from flask.ext.script import Server, Manager
 import os
 
-port = 5000
-if 'PORT' in os.environ.keys():
-    port = os.environ['PORT']
+#port = 5000
+#if 'PORT' in os.environ.keys():
+#    port = os.environ['PORT']
+port = int(os.getenv("PORT"))
 
 manager = Manager(app)
-manager.add_command("runserver", Server(port=port))
+manager.add_command("runserver", Server(host='0.0.0.0', port=port))
 
 
 @manager.command
